ac57be7e8c8fce782c1fc74f84a54f6c0f66320c
Hien Le <lequanghien247@gmail.com>
2024-05-13 23:31:14 +0200

    Fix wrong native coroutines annotation

--- shared/src/commonMain/kotlin/com/hien/mykmm/viewmodels/RocketViewModel.kt
+++ shared/src/commonMain/kotlin/com/hien/mykmm/viewmodels/RocketViewModel.kt
@@ -4,12 +4,12 @@ import com.hien.mykmm.data.RocketLaunch
 import com.hien.mykmm.data.SpaceXRepository
 import com.rickclephas.kmm.viewmodel.KMMViewModel
 import com.rickclephas.kmm.viewmodel.stateIn
-import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 
 class RocketViewModel(spaceXRepository: SpaceXRepository) : KMMViewModel() {
-    @NativeCoroutines
+    @NativeCoroutinesState
     val objects: StateFlow<List<RocketLaunch>> =
         spaceXRepository.getObjects()
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
